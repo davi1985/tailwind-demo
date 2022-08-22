ce7795779e5c1b8415e89aee98ee9815991dabfc
Davi Silva <davisilvaphoto@gmail.com>
2022-08-22 17:47:57 -0300

    tailwind demo app

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import { CardInfoProps } from "./@types/types";
+import { Card } from "./components/Card";
+import { CardInfo } from "./components/CardInfo";
+import { Form } from "./components/Form";
+
+export const App = () => {
+  const [cardInfo, setCardInfo] = useState<CardInfoProps>({
+    nameOnCard: "",
+    cardNumber: "",
+    expiry: "",
+    cvv: undefined,
+  });
+
+  return (
+    <Card>
+      <CardInfo cardInfo={cardInfo} />
+
+      <h1 className="font-light tracking-wide text-2xl mb2">
+        Update payment method
+      </h1>
+
+      <p className="text-gray-500 mb-4 text-lg">Update your card details</p>
+
+      <Form cardInfo={cardInfo} setCardInfo={setCardInfo} />
+    </Card>
+  );
+};
